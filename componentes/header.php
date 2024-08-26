170e2534464fbe0ab22b2a5247e67afc0d6ab927
TheBest1959 <masterorigenmedios@gmail.com>
2024-08-26 20:07:16 +0000

    validaciones

--- componentes/header.php
+++ componentes/header.php
@@ -14,7 +14,7 @@ $nombre = $_SESSION['user_name'];
 
 
 
-$ruta = 'https://refactored-space-guacamole-r4rqv4jjq7wxfwpvx-3000.app.github.dev/';
+$ruta = 'https://cautious-space-rotary-phone-pjg4qjxx4g5xhwx-3000.app.github.dev/';
 
 
 
